12e9594e7e863f20cc720a727040dfd86a84ab9d
Vexarian <julianpasker@gmail.com>
2024-11-06 12:12:50 +0100

    Initial setup

---
+++ backend/pokeforge/src/test/java/com/vexarian/pokeforge/PokeforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vexarian.pokeforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
