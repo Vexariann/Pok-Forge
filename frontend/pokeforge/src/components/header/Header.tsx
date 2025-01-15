ff9fd76280371f9f7ca9551b8d1d68fa38f55392
Vexarian <julianpasker@gmail.com>
2025-01-15 13:45:24 +0100

    Added images

---
+++ frontend/pokeforge/src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <>
+      <b>Test header</b>
+    </>
+  );
+}
---
+++ frontend/pokeforge/src/components/home/Home.tsx
@@ -0,0 +1,9 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <>
+      <b>Test Home</b>
+    </>
+  );
+}
