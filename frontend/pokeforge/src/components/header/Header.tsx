0eb13a21d225ea42526589bfb5a688c79a1d7f27
Vexarian <julianpasker@gmail.com>
2025-01-15 15:56:16 +0100

    prettied up homepage

--- frontend/pokeforge/src/components/header/Header.tsx
+++ frontend/pokeforge/src/components/header/Header.tsx
@@ -3,7 +3,18 @@ import "./Header.css";
 export default function Header() {
   return (
     <>
-      <b>Test header</b>
+      <div className="header">
+        <img
+          className="logo"
+          alt="PokeForge Logo"
+          src="../src/assets/UI/PokeForgeLogo.png"
+        ></img>
+        <div className="headerItem">Teams</div>
+        <div className="headerItem">Resources</div>
+        <div className="headerItem">Damage Calc</div>
+        <div className="headerItem">News</div>
+        <div className="headerItem lastHeaderItem">Login</div>
+      </div>
     </>
   );
 }
--- frontend/pokeforge/src/components/home/Home.tsx
+++ frontend/pokeforge/src/components/home/Home.tsx
@@ -3,7 +3,14 @@ import "./Home.css";
 export default function Home() {
   return (
     <>
-      <b>Test Home</b>
+      <div className="splashImage">
+        <img
+          className="splashImage"
+          alt="regulation_g_en"
+          src="../src/assets/UI/regulation_g_en.jpg"
+        ></img>
+      </div>
+      <div>test text</div>
     </>
   );
 }
